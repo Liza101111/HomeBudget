b59207968e4d92f4c7f2ca21a5aaef3576e73d58
Liza <lixia101111@gmail.com>
2022-10-24 12:46:52 +0300

    [change]-package name

--- src/main/java/com/finalproject/homebudget/HomeBudgetApplication.java
+++ src/main/java/com/finalproject/homebudget/HomeBudgetApplication.java
@@ -1,4 +1,4 @@
-package com.example.homebudget;
+package com.finalproject.homebudget;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/finalproject/homebudget/HomeBudgetApplicationTests.java
+++ src/test/java/com/finalproject/homebudget/HomeBudgetApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.homebudget;
+package com.finalproject.homebudget;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
